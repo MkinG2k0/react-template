b720beeefac7637176f30c635e024848d19d9131
Mking <kamai1220000@gmail.com>
2022-11-24 23:28:18 +0300

    replace redux hook useStore

--- src/Redux/Store/Redux.hooks.ts
+++ src/Redux/Store/Redux.hooks.ts
@@ -1,23 +1,4 @@
-import { useEffect, useState } from 'react'
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
 import type { RootState } from 'Redux/Store/IRootSate'
 
-import { store } from './store'
-
-export function useStoreWithInitializer<T>(
-	getter: (state: RootState) => T,
-	initializer: () => void,
-) {
-	const [state, setState] = useState(getter(store.getState()))
-	useEffect(() => {
-		const unsubscribe = store.subscribe(() =>
-			setState(getter(store.getState())),
-		)
-		initializer()
-		return unsubscribe
-	}, [getter, initializer])
-	return state
-}
-
-export function useStore<T>(getter: (state: RootState) => T) {
-	return useStoreWithInitializer(getter, () => {})
-}
+export const useStore: TypedUseSelectorHook<RootState> = useSelector
