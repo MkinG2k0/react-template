65d17b3378be0585c0e879a4f04040c4000eeed2
Mking <kamai1220000@gmail.com>
2023-01-21 17:42:26 +0300

    run lint

--- src/Components/GG/GG.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-import style from './GG.module.scss'
-
-interface GGProps {}
-
-export const GG: FC<GGProps> = ({}) => {
-	return <div className={style.wrap}>GG</div>
-}
